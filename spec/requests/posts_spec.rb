3701303ba61a56b050aa34e5ae2dd9ad78b9afee
Kurt Nelson <kurtisnelson@gmail.com>
2015-10-28 09:50:20 -0500

    Create post model

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,9 +2,11 @@ require 'request_helper'
 
 RSpec.describe "posts" do
   describe "#index" do
-    it "doesn't crash" do
+    it "shows posts" do
+      Post.create!(title: "My Post", body: "important things")
       get posts_path
       expect(response).to have_http_status(:success)
+      expect(json).to eq []
     end
   end
 end
