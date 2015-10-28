00208953aefa28b26a3db45c4793caaabc58dbd4
Kurt Nelson <kurtisnelson@gmail.com>
2015-10-28 09:38:44 -0500

    Empty posts route

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,5 +1,10 @@
 require 'request_helper'
 
-RSpec.describe 'posts' do
-
+RSpec.describe "posts" do
+  describe "#index" do
+    it "doesn't crash" do
+      get posts_path
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
